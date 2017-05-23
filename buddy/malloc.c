d2b6fbb3b73843937ee2460c3c2b1b121d3069fe
hannushka <lindbladhannah@gmail.com>
2017-05-23 23:25:05 +0200

    Pseudo code for buddy

--- buddy/malloc.c
+++ buddy/malloc.c
@@ -6,10 +6,31 @@
 #include <stdio.h>
 #include <stddef.h>
 
+#define N 1024 //Memory size
+
 //Data structures
+
+typedef struct list_t list_t;
+
+struct list_t {
+	size_t  size; //size including list_t
+	list_t* next; //next available block.
+	int free;
+	char    data[]; //C99 flexible array.
+};
+
+/*Keep a linked list (n) for each possible memory size 2^n == memory size.
+All empty except for the largest memory size which has a block*/
+void* list[N];
 
 void *malloc(size_t size)
 {
+	/*Round size up to power of two = y
+	Check if available block of resulting size by taking the log base 2 of y
+	In C you can write = log(x)/log(2)
+	to get the free list number.
+	Otherwise check upwards in sizes, splitting each in two
+	If no space return null*/
 }
 
 void *calloc(size_t nitems, size_t size)
@@ -22,4 +43,7 @@ void *realloc(void *ptr, size_t size)
 
 void free(void *ptr)
 {
+	/*If buddy also free => merge the blocks and recurse until no free buddy
+	add the resulting block to list
+	Else add block to list*/
 }
