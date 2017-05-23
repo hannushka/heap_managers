6bcffe5e5b9d79f3aa1c3fb7f80eb4354aaddeee
Jan Svensson <jsvensson51@yahoo.com>
2017-05-23 12:36:56 +0200

    Add includes in Buddy System

--- buddy/malloc.c
+++ buddy/malloc.c
@@ -1,5 +1,13 @@
 //Buddy system version
 
+#include <unistd.h>
+#include <string.h>
+#include <stdlib.h>
+#include <stdio.h>
+#include <stddef.h>
+
+//Data structures
+
 void *malloc(size_t size)
 {
 }
