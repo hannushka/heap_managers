85bbaa90e3b3b52ab340d6858908d77dc4b9433b
hannushka <lindbladhannah@gmail.com>
2017-05-24 11:08:46 +0200

    Fix realloc linked list impl

--- linked_list/malloc.c
+++ linked_list/malloc.c
@@ -11,7 +11,7 @@ typedef struct list_t list_t;
 struct list_t {
 	size_t  size; //size including list_t
 	list_t* next; //next available block.
-	int free;
+	unsigned int free; //if available or not
 	char    data[]; //C99 flexible array.
 };
 
@@ -22,12 +22,12 @@ list_t *request_space(list_t* last, size_t size)
 {
 	list_t* block = sbrk(0);
 	void* req = sbrk(size + LIST_T_SIZE);
-	if (req == (void*) -1) {
+	if (req == (void*) -1)
 		return NULL;
-	}
-	if (last) {
+
+	if (last)
 		last->next = block;
-	}
+
 	block->size = size;
 	block->next = NULL;
 	block->free = 0;
@@ -51,18 +51,17 @@ void *malloc(size_t size)
 		return NULL;
 	if (!global_head) {
 		block = request_space(NULL, size);
-		if (!block) {
+
+		if (!block)
 			return NULL;
-		}
 		global_head = block;
 	} else {
 		list_t* last = global_head;
 		block = find_free_block(&last, size);
 		if (!block) {
 			block = request_space(last, size);
-			if (!block) {
+			if (!block)
 				return NULL;
-			}
 		} else {
 			block->free = 0;
 		}
@@ -75,20 +74,17 @@ void *calloc(size_t nitems, size_t size)
 	void *memory = malloc(nitems * size);
 
 	if(memory != NULL)
-	{
 		memset(memory, 0, nitems * size);
-	}
 
-	return *memory;
+	return memory;
 }
 
 void *realloc(void *ptr, size_t size)
 {
 	void *new_memory = malloc(size);
 
-	if(new_memory != NULL)
-	{
-		memmove(new_memory, ptr, size_t __len, size);
+	if (new_memory != NULL) {
+		memmove(new_memory, ptr, size);
 		free(ptr);
 	}
 	return new_memory;
@@ -96,9 +92,8 @@ void *realloc(void *ptr, size_t size)
 
 void free(void *ptr)
 {
-	if (!ptr) {
+	if (!ptr)
 		return;
-	}
 	list_t* block_ptr = (list_t*)ptr-1;
 	block_ptr->free = 1;
 }
